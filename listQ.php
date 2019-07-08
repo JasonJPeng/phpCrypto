21c2e378db4ab0c2d026bd74e2399dd4853cdb6e
Jason Peng <jason.j.peng@gamil.com>
2019-07-08 07:51:18 -0700

    4000 crypto added

--- listQ.php
+++ listQ.php
@@ -3,6 +3,11 @@
 //   https://min-api.cryptocompare.com/data/all/coinlist
 //   https://www.cryptocompare.com/media/19702/ezc.png
 
+$fileName = "listQ.js";
+
+$myfile = fopen($fileName, "w") or die("Unable to open file!");
+
+fwrite($myfile, "listQ = [ \n");
 
 // create a new cURL resource
 $ch = curl_init();
@@ -18,13 +23,16 @@ $j = json_decode(curl_exec($ch));
 $coinNames = get_object_vars($j->Data);
 
 foreach ($coinNames as $key=>$value) {
-    echo $value->CoinName . "    " . $value->ImageUrl . "\n<br>";
+    // echo $value->CoinName . "    " . $value->ImageUrl . "\n<br>";
+    fwrite($myfile, '["' . $value->CoinName .   '",  ' . '"https://www.cryptocompare.com' . $value->ImageUrl . '"],' .PHP_EOL);
 }
 
-print_r($coinNames);
+fwrite($myfile, "];");
+
+// print_r($coinNames);
 // var_dump($j);
 
-
+fclose($myfile);
 // close cURL resource, and free up system resources
 curl_close($ch);
 
